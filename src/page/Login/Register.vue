eeb6819a69c9b8246332d34617224c4db5ccaeaa
junyi <1029842003@qq.com>
2020-01-23 21:29:24 +0800

    加入表单验证+提示信息

--- src/page/Login/Register.vue
+++ src/page/Login/Register.vue
@@ -1,28 +1,30 @@
 <template>
   <div class="register">
     <van-nav-bar
-      title="标题"
+      title="注册"
       left-text=""
       left-arrow
       @click-left="onClickLeft"
     />
     <div class="registerform">
       <div class="inp">
-        <input type="text" placeholder="请输入手机号" class="phoneNum">
+        <input type="text" placeholder="请输入手机号" class="phoneNum" maxlength="11"  v-model="phoneNum" @blur="useNameBlur" @input="useNameChange">
+        <span v-if="phoneErr">{{phoneErrMsg}}</span>
       </div>
       <div class="inp">
-        <input type="text" placeholder="请输入验证码" >
-        <van-button plain round type="warning" class="get" size="mini" >获取验证码</van-button>
+        <input type="text" placeholder="请输入验证码" maxlength="6">
+        <van-button plain round type="warning" class="get" size="mini">获取验证码</van-button>
       </div>
       <div class="inp">
-        <input type="password" placeholder="password" >
+        <input type="password" placeholder="password" maxlength="12" v-model="password" @input="passwordChange">
+        <span v-if="passErr">{{passErrMsg}}</span>
       </div>
     
     
     <div class="read">
       <van-checkbox class="check" icon-size=".25rem" v-model="checked"></van-checkbox><p>已阅读并同意<span>《用户服务协议》</span></p>
     </div>
-     <van-button :disabled="disable" round type="warning" class="login-btn">登录</van-button>
+     <van-button :disabled="disable" round type="warning" class="login-btn">注册</van-button>
     </div>
   </div>
   
@@ -36,7 +38,13 @@ export default {
   data() {
     return {
       checked:true,
-      disable:true
+      disable:true,
+      phoneErr:null,
+      passErr:null,
+      phoneNum:'',
+      password:'',
+      phoneErrMsg:'',
+      passErrMsg:''
     }
   },
   components:{
@@ -49,7 +57,54 @@ export default {
       console.log('返回');
       this.$router.push("login")
     },
+    useNameChange(){
+      if(this.phoneNum.length===11){
+        
+        //正则判断
+        if(/^1[3578]\d{9}$/.test(this.phoneNum)){
+          this.phoneErr = false
+        }else{
+          this.phoneErr=true
+          this.phoneErrMsg='请输入正确手机号'
+        }
+        
+      }else if(this.phoneNum.length===0){
+        this.phoneErr=true
+        this.phoneErrMsg='手机号不能为空'
+      }else{
+        this.phoneErr=true
+        this.phoneErrMsg='请输入11位手机号'
+      }
+    },
+    useNameBlur(){
+      if(!this.phoneErr){
+        //如果手机号不为空，且通过正则判断，则发起axios请求检测手机号是否存在
+        console.log('axios请求')
+      }
+      
+    },
+    passwordChange(){
+      console.log('改变')
+      let count = 0;
+      if(this.password.length>=6){
+        if(/\d+/.test(this.password)&&/[a-zA-Z]+/.test(this.password)){
+          this.passErr=false
+          
+        }else{
+          this.passErr=true
+          this.passErrMsg='必须包含数字和字母'
+        }
+      }else{
+        this.passErr=true
+        this.passErrMsg='请输入6位以上密码'
+      }
+    }
   },
+  updated(){
+    if(this.passErr===false&&this.phoneErr===false){
+      this.disable=false
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -70,6 +125,11 @@ export default {
           margin-top: 0.5rem;
         input::-webkit-input-placeholder
           color #bbb
+        >span 
+          position absolute
+          left 4rem
+          top .6rem
+          color red
         .get
           position: absolute;
           right: 0;
@@ -85,7 +145,7 @@ export default {
         .check
           margin-right .2rem
         span 
-          color red
+          color blue
       .login-btn
         margin-top .6rem
         height 1.1rem
